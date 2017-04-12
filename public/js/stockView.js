693eac4c87060438ad507944ef9efcf3f80a7d29
Glen Taylor <deepnsnow@gmail.com>
2017-04-12 02:29:51 -0600

    changed the location of the graph to the stockView.js file, set up a start for more advanced functionality for the graph

--- public/js/stockView.js
+++ public/js/stockView.js
@@ -1,137 +1,50 @@
-/*
-The purpose of this demo is to demonstrate how multiple charts on the same page can be linked
-through DOM and Highcharts events and API methods. It takes a standard Highcharts config with a
-small variation for each data set, and a mouse/touch event handler to bind the charts together.
-*/
+window.onload = () => {
+        $(function(){
+            var dict = stockDict;
+            var value = 0;
+        });
 
-$(function () {
-
-    /**
-     * In order to synchronize tooltips and crosshairs, override the
-     * built-in events with handlers defined on the parent element.
-     */
-    $('#container').bind('mousemove touchmove touchstart', function (e) {
-        var chart,
-            point,
-            i,
-            event;
-
-        for (i = 0; i < Highcharts.charts.length; i = i + 1) {
-            chart = Highcharts.charts[i];
-            event = chart.pointer.normalize(e.originalEvent); // Find coordinates within the chart
-            point = chart.series[0].searchPoint(event, true); // Get the hovered point
-
-            if (point) {
-                point.highlight(e);
-            }
-        }
-    });
-    /**
-     * Override the reset function, we don't need to hide the tooltips and crosshairs.
-     */
-    Highcharts.Pointer.prototype.reset = function () {
-        return undefined;
-    };
-
-    /**
-     * Highlight a point by showing tooltip, setting hover state and draw crosshair
-     */
-    Highcharts.Point.prototype.highlight = function (event) {
-        this.onMouseOver(); // Show the hover marker
-        this.series.chart.tooltip.refresh(this); // Show the tooltip
-        this.series.chart.xAxis[0].drawCrosshair(event, this); // Show the crosshair
-    };
-
-    /**
-     * Synchronize zooming through the setExtremes event handler.
-     */
-    function syncExtremes(e) {
-        var thisChart = this.chart;
-
-        if (e.trigger !== 'syncExtremes') { // Prevent feedback loop
-            Highcharts.each(Highcharts.charts, function (chart) {
-                if (chart !== thisChart) {
-                    if (chart.xAxis[0].setExtremes) { // It is null while updating
-                        chart.xAxis[0].setExtremes(e.min, e.max, undefined, false, { trigger: 'syncExtremes' });
-                    }
-                }
-            });
-        }
-    }
-
-    // Get the data. The contents of the data file can be viewed at
-    // https://github.com/highcharts/highcharts/blob/master/samples/data/activity.json
-    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=activity.json&callback=?', function (activity) {
-        $.each(activity.datasets, function (i, dataset) {
-
-            // Add X values
-            dataset.data = Highcharts.map(dataset.data, function (val, j) {
-                return [activity.xData[j], val];
-            });
-
-            $('<div class="chart">')
-                .appendTo('#container')
-                .highcharts({
-                    chart: {
-                        marginLeft: 40, // Keep all charts left aligned
-                        spacingTop: 20,
-                        spacingBottom: 20,
-                        zoomType: 'x'
-                    },
+        function makeChart(){
+                Highcharts.chart('container', {
                     title: {
-                        text: dataset.name,
-                        align: 'left',
-                        margin: 0,
-                        x: 30
+                        text: 'My stock performance'
                     },
-                    credits: {
-                        enabled: false
-                    },
-                    legend: {
-                        enabled: false
-                    },
-                    xAxis: {
-                        crosshair: true,
-                        events: {
-                            setExtremes: syncExtremes
-                        },
-                        labels: {
-                            format: '{value} km'
-                        }
+                    /*subtitle: {
+                        text: 'Source: thesolarfoundation.com'
                     },
                     yAxis: {
                         title: {
-                            text: null
+                            text: 'Growth index
                         }
+                    },*/
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle'
                     },
-                    tooltip: {
-                        positioner: function () {
-                            return {
-                                x: this.chart.chartWidth - this.label.width, // right aligned
-                                y: -1 // align to title
-                            };
-                        },
-                        borderWidth: 0,
-                        backgroundColor: 'none',
-                        pointFormat: '{point.y}',
-                        headerFormat: '',
-                        shadow: false,
-                        style: {
-                            fontSize: '18px'
-                        },
-                        valueDecimals: dataset.valueDecimals
+                    plotOptions: {
+                        series: {
+                            pointStart: 2010
+                        }
                     },
                     series: [{
-                        data: dataset.data,
-                        name: dataset.name,
-                        type: dataset.type,
-                        color: Highcharts.getOptions().colors[i],
-                        fillOpacity: 0.3,
-                        tooltip: {
-                            valueSuffix: ' ' + dataset.unit
-                        }
+                        name: 'Installation',
+                        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+                    }, {
+                        name: 'Manufacturing',
+                        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+                    }, {
+                        name: 'Sales & Distribution',
+                        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+                    }, {
+                        name: 'Project Development',
+                        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+                    }, {
+                        name: 'Other',
+                        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
                     }]
                 });
-        });
-    });
-});
+            }
+
+            makeChart();
+}
