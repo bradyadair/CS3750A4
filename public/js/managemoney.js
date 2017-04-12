e76ad35a534fe3447b6b4062531aef07c2cfa4c3
bradyadair <coolguy043@hotmail.com>
2017-04-11 20:35:05 -0600

    data can be passed through route now

--- public/js/managemoney.js
+++ public/js/managemoney.js
@@ -1,63 +1,47 @@
 window.onload = () => {
     $(function(){
-            $("#slider").slider();
-            $("#slider2").slider({
-                change:function( event, ui ) { var value = $( "#slider2" ).slider( "option", "value" ); console.log("Hello " +value);}
-            });
-            });
+        var dict = stockdict;
+        
 
-            //defining a function
-            function makeChart(){
-                Highcharts.chart('container', {
-                chart: {
-                    plotBackgroundColor: null,
-                    plotBorderWidth: null,
-                    plotShadow: false,
-                    type: 'pie'
-                },
-                title: {
-                    text: 'Browser market shares January, 2015 to May, 2015'
-                },
-                tooltip: {
-                    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-                },
-                plotOptions: {
-                    pie: {
-                        allowPointSelect: true,
-                        cursor: 'pointer',
-                        dataLabels: {
-                            enabled: false
-                        },
-                        showInLegend: true
-                    }
-                },
-                series: [{
-                    name: 'Brands',
-                    colorByPoint: true,
-                    data: [{
-                        name: 'Microsoft Internet Explorer',
-                        y: 56.33
-                    }, {
-                        name: 'Chrome',
-                        y: 24.03,
-                        sliced: true,
-                        selected: true
-                    }, {
-                        name: 'Firefox',
-                        y: 10.38
-                    }, {
-                        name: 'Safari',
-                        y: 4.77
-                    }, {
-                        name: 'Opera',
-                        y: 0.91
-                    }, {
-                        name: 'Proprietary or Undetectable',
-                        y: 0.2
-                    }]
-                }]
-                });
-            }
+        $("#slider").slider();
+        $("#slider2").slider({
+            change:function( event, ui ) { value = $( "#slider2" ).slider( "option", "value" ); console.log("Hello " +value); makeChart(dict);}
+        });
+        
 
-            makeChart();
+        //defining a function
+        function makeChart(mydict){
+            Highcharts.chart('container', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: 'Browser market shares January, 2015 to May, 2015'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                name: 'Brands',
+                colorByPoint: true,
+                data: mydict
+            }]
+            });
+        }
+
+        makeChart(dict);
+        });
 }
--- routes/stock.js
+++ routes/stock.js
@@ -51,8 +51,30 @@
 
     var sess = req.session;
     var decodedToken = jwt.verify(sess.token, 'secret');
+
+    var dict = [{
+                    name: 'Microsoft Internet Explorer',
+                    y: 33.1
+                }, {
+                    name: 'Chrome',
+                    y: 24.03,
+                    sliced: true,
+                    selected: true
+                }, {
+                    name: 'Firefox',
+                    y: 10.38
+                }, {
+                    name: 'Safari',
+                    y: 4.77
+                }, {
+                    name: 'Opera',
+                    y: 0.91
+                }, {
+                    name: 'Proprietary or Undetectable',
+                    y: 0.2
+                }];
     
-    res.render('managemoney');
+    res.render('managemoney', {dict:dict});
   });
 
 module.exports = router;
